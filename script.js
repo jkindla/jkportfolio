94bf7ccb7ac9b23b517768fe7251b598f2872f80
jkindla <82672312+jkindla@users.noreply.github.com>
2024-11-05 11:49:40 -0600

    edits

--- script.js
+++ script.js
@@ -108,33 +108,28 @@ $(document).ready(function () {
   });
 
   // Ajax form
-  var form = $("#ajax-contact");
-  var formMessages = $("#form-messages");
+// AJAX form
+var form = $("#ajax-contact");
+var formMessages = $("#form-messages");
 
-  $(form).submit(function (e) {
+$(form).submit(function (e) {
     e.preventDefault();
     var formData = $(form).serialize();
     $.ajax({
-      type: "POST",
-      url: $(form).attr("action"),
-      data: formData,
+        type: "POST",
+        url: $(form).attr("action"),
+        data: formData,
     })
-      .done(function (response) {
+    .done(function () {
         $(formMessages).removeClass("error");
         $(formMessages).addClass("success");
-        $(formMessages).text(response);
+        $(formMessages).text("Thank you! Your message has been sent successfully.");
         $("#name, #email, #message").val("");
-      })
-      .fail(function (data) {
+    })
+    .fail(function () {
         $(formMessages).removeClass("success");
         $(formMessages).addClass("error");
-        if (data.responseText !== "") {
-          $(formMessages).text(data.responseText);
-        } else {
-          $(formMessages).text(
-            "Oops! An error occurred, and your message could not be sent."
-          );
-        }
-      });
-  });
+        $(formMessages).text("Oops! An error occurred, and your message could not be sent.");
+    });
+});
 });
